83636653d420b923ba70d5bf3c0310ae8eaee496
najeeb <naj404@hotmail.com>
2017-10-17 00:03:29 -0500

    my first js commit

---
+++ maze.js
@@ -0,0 +1,6 @@
+<script>
+document.getElementById("maze").addEventListener("load", myFunction);
+function myFunction() {
+	document.getElementByID('boundary1').mouseover=setSttribute("id", "youlose");
+}
+</script>
