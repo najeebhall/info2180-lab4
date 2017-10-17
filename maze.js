7ca3e55c4a3ebd31591e51700ea83c3d4c98aeba
najeeb <naj404@hotmail.com>
2017-10-17 13:21:56 -0500

    my second js commit

--- maze.js
+++ maze.js
@@ -1,6 +1,16 @@
-<script>
-document.getElementById("maze").addEventListener("load", myFunction);
-function myFunction() {
-	document.getElementByID('boundary1').mouseover=setSttribute("id", "youlose");
-}
-</script>
+
+window.onload = function () {
+	var walls = document.querySelectorAll(".boundary");
+	var wall = document.getElementById("boundary1");
+	wall.addEventListener('mouseover', function()
+		{ this.setAttribute('class', 'boundary youlose')
+	});
+	//document.getElementsByClassName("boundary").mouseover.setAttribute("id", "youlose");
+};
+
+//document.getElementById("maze").addEventListener("load", myFunction);
+//function myFunction() {
+//	wall.mouseover.setAttribute("class", "boundary youlose");
+//}
+
+
