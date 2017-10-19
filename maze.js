c8433acf5be637c0812250f1f61809e4c1ea3de3
najeeb <naj404@hotmail.com>
2017-10-19 15:44:14 -0500

    my fourth js commit

--- maze.js
+++ maze.js
@@ -2,25 +2,31 @@
 window.onload = function () {
 	var walls = document.querySelectorAll(".boundary");
 	var wall = document.getElementById("boundary1");
+	var end = document.getElementById("end");
 	wall.addEventListener('mouseover', function()
 		{ this.setAttribute('class', 'boundary youlose')
 	});
 	for (var i = 0; i < walls.length; i++) {
 		walls[i].addEventListener("mouseover",function(){lose(walls)});
 	}
-	//document.getElementsByClassName("boundary").mouseover.setAttribute("id", "youlose");
+	end.addEventListener("mouseover",function(){endGame(walls)});
+	
 };
 function lose(elem){
-	gameLost=false;
-	//document.getElementById("status").innerHTML="Move your mouse over the 'S' to begin."; 
+	gameLost=false; 
 	for (var i = 0; i <= elem.length; i++) {
 		elem[i].setAttribute("class","boundary youlose");
 	}
 }
 
-//document.getElementById("maze").addEventListener("load", myFunction);
-//function myFunction() {
-//	wall.mouseover.setAttribute("class", "boundary youlose");
-//}
+function endGame(elem)
+{
+	var status = elem[0].getAttribute('class');
+	if (status=== 'boundary') {
+			alert('you win!');
+	}
+}
+
+
 
 
