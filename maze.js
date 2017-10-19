2bfb423f7482f68d5851cddda7c1a987d518f461
najeeb <naj404@hotmail.com>
2017-10-19 16:17:32 -0500

    my fifth js commit

--- maze.js
+++ maze.js
@@ -2,6 +2,8 @@
 window.onload = function () {
 	var walls = document.querySelectorAll(".boundary");
 	var wall = document.getElementById("boundary1");
+	var start = document.getElementById("start");
+	start.addEventListener('click', function() {startGame(walls)});
 	var end = document.getElementById("end");
 	wall.addEventListener('mouseover', function()
 		{ this.setAttribute('class', 'boundary youlose')
@@ -14,7 +16,7 @@ window.onload = function () {
 };
 function lose(elem){
 	gameLost=false; 
-	for (var i = 0; i <= elem.length; i++) {
+	for (var i = 0; i < elem.length; i++) {
 		elem[i].setAttribute("class","boundary youlose");
 	}
 }
@@ -24,6 +26,17 @@ function endGame(elem)
 	var status = elem[0].getAttribute('class');
 	if (status=== 'boundary') {
 			alert('you win!');
+	}
+}
+
+function startGame(elem)
+{
+	var status = elem[0].getAttribute('class');
+	if (status=== 'boundary youlose')
+	{
+		for (var i = 0; i < elem.length; i++) {
+		elem[i].classList.remove("youlose");
+		}
 	}
 }
 
